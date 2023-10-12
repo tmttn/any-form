2534da5acaeae64addd165b4a91fd5aaca4ca148
Thomas Metten <tom.metten@acagroup.be>
2023-10-12 17:25:36 +0200

    feat: add submit function

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -24,4 +24,19 @@ const { data: content } = await useAsyncData('builderData', () =>
         },
     })
 );
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => alert("Thank you for your submission!"))
+    .catch((error) => alert(error));
+};
 </script>
